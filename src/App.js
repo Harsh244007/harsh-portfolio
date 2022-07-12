b9d55fa2536a2aa1a58260efd808552e22a13529
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-07-12 21:15:38 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
-import React,{Suspense} from "react"
+import React, { Suspense } from "react"
 import './App.scss'
+import Loader from "react-loaders"
 import { Route, Routes } from 'react-router-dom'
-const Home=React.lazy(()=>import("./components/Home"))
-const About=React.lazy(()=>import("./components/About"))
-const Contact=React.lazy(()=>import("./components/Contact"))
-const Layout=React.lazy(()=>import("./components/Layout"))
-const Portfolio=React.lazy(()=>import("./components/Portfolio"))
-const ErrorPage=React.lazy(()=>import("./components/ErrorPages"))
-
+const Home = React.lazy(() => import("./components/Home"))
+const About = React.lazy(() => import("./components/About"))
+const Contact = React.lazy(() => import("./components/Contact"))
+const Layout = React.lazy(() => import("./components/Layout"))
+const Portfolio = React.lazy(() => import("./components/Portfolio"))
+const ErrorPage = React.lazy(() => import("./components/ErrorPages"))
 
 function App() {
   return (
     <>
-    {/* //   <Main  />
+      {/* //   <Main  />
        */}
- 
+
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={
-          <Suspense fallback={<h1>About page is loading</h1>}>
-          <Home />
-          </Suspense>}/>
+          <Route index element={<Home />} />
           <Route path="/about" element={
-          <Suspense fallback={<h1>About page is loading</h1>}>
 
-          <About />
-    </Suspense>}/>
-         
-          
+            <Suspense fallback={<Loader type="pacman" />}>
+
+              <About />
+            </Suspense>} />
+
+
           <Route path="/contact" element={
-          <Suspense fallback={<h1>Contact page is loading</h1>}>
-          <Contact />
-         </Suspense>}/> 
-       
-        
+
+            <Suspense fallback={<Loader type="pacman" />}>
+              <Contact />
+            </Suspense>} />
+
+
           <Route path="/portfolio" element={
-            <Suspense fallback={<h1>Portfolio is loading</h1>}>
-          <Portfolio />
-          
-        </Suspense>}/> 
-         
+
+            <Suspense fallback={<Loader type="pacman" />}>
+              <Portfolio />
+
+            </Suspense>} />
+
 
           <Route path="*" element={
-          <Suspense fallback={<h1>Error page</h1>}>
-                    
-          <ErrorPage />
-          
-          </Suspense>
+
+            <Suspense fallback={<Loader type="pacman" />}>
+
+              <ErrorPage />
+
+            </Suspense>
           } />
         </Route>
       </Routes>
